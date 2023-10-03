43bb03774c73084c7317fd15f43f46b2b9ce7333
Thiwanka Somachandra <thivankas@outlook.com>
2023-10-03 11:50:39 +0300

    Setting up the initial stage for the excercise

---
+++ Part1/unicafe/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+const App = () => {
+  
+  return (
+    <div>
+      <p>initial stage</p>
+    </div>
+  )
+}
+
+export default App
