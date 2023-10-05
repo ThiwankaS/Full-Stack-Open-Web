97c240e44aa0844c66311e6bf5e42c721dd40ede
Thiwanka Somachandra <thivankas@outlook.com>
2023-10-05 18:05:38 +0300

    2.1 Excercise Course information step6

--- Part2/courseinfo/src/App.jsx
+++ Part2/courseinfo/src/App.jsx
@@ -2,12 +2,62 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
-const App = () => {
+
+const App = ()  => {
+  
+  const course = {
+    id : 1,
+    name : 'Half Stack application development',
+    parts : [
+      {
+        name : 'Fundamentals of react', 
+        exercises : 10, 
+        id : 1
+      },
+      {
+        name : 'Using props to pass data',
+        exercises : 7,
+        id : 2
+      },
+      {
+        name : 'State of a component',
+        exercises : 14,
+        id : 3
+      }
+    ]
+  }
+
+  return <Course course={course} /> 
+}
+
+export default App;
+
+const Course = ({course}) => {
   return (
     <div>
-      <h1>Intitial stage</h1>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
     </div>
   )
 }
 
-export default App; 
+const Header = ({course}) => <h1>{course}</h1>
+
+const Content = (props) => {  
+  const {parts} = props; 
+  return (
+    <div>
+    {
+      parts.map(part =><Part key={part.id} name={part.name} exercises={part.exercises}/>)
+    }
+    </div>
+  )
+}
+
+const Part = ({name,exercises}) => <p>{name} {exercises}</p> 
+
+
+
+
+
+
