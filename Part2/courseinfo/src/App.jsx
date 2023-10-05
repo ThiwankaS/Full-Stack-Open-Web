d067853dcf6f1c4cac59d6252edc22972cdb814f
Thiwanka Somachandra <thivankas@outlook.com>
2023-10-05 09:25:48 +0300

    Setting the initial stage for the excercise

---
+++ Part2/courseinfo/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Intitial stage</h1>
+    </div>
+  )
+}
+
+export default App; 
