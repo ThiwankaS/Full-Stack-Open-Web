edbde9c6bf3e653c5ca13516bdd88b50e9ccb6b4
Thiwanka Somachandra <thivankas@outlook.com>
2023-10-06 21:44:53 +0300

    2.8 Exercise The Phonebook Step3

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -3,35 +3,43 @@ import { useState } from 'react'
 const App = () => {
 
   const [ person,setPerson ] = useState([
-    {name: 'Alto Hellas'},
-    {name: 'Ada Lovelace'}
+    { 
+      name : 'Alto Hellas',
+      number : '040-123456' 
+    },
+    {
+      name : 'Ada Lovelace',
+      number : '39-44-5323523' 
+    }
   ]); 
 
   const [ newName,setNewName ] = useState(''); 
+  const [ newNumber,setNewNumber ] = useState('');
 
-  const handelchange = (event) => {
+  const handelNameChange = (event) => {
     setNewName(event.target.value); 
   }
 
+  const handelNumberChange = (event) => {
+    setNewNumber(event.target.value); 
+  }
+
   const addPerson = (event) => {
-
     event.preventDefault(); 
-
-    const alreadyExist = (person.some(person => person.name === newName)); 
-
-    if(alreadyExist){
-
-      alert(`${newName} is already added to phonebook`);
+    const nameAlreadyExist = (person.some(person => person.name === newName)); 
+    const numberAlreadyExist = (person.some(person => person.number === newNumber));
+    if(nameAlreadyExist || numberAlreadyExist){
+      nameAlreadyExist ? alert(`${newName} is already added to phonebook`): alert(`${newNumber} is already added to phonebook`);
       setNewName('');
-
+      setNewNumber('');
     } else {
-
       const newPerson = {
-        name: newName
+        name: newName,
+        number : newNumber
       }
-      
       setPerson(person.concat(newPerson)); 
       setNewName(''); 
+      setNewNumber('');
     }
   }
 
@@ -40,13 +48,13 @@ const App = () => {
     <div>
       <h2>Phone Book</h2>
       <form onSubmit={addPerson}>
-        <div> name : <input value={newName} onChange={handelchange}/> </div>
+        <div> name : <input value={newName} onChange={handelNameChange}/> </div>
+        <div> number : <input value={newNumber} onChange={handelNumberChange}/> </div>
         <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-      {person.map((person)=><p key={person.name}>{person.name}</p>)}
+      {person.map((person)=><p key={person.name}>{person.name} : {person.number}</p>)}
       <br/>
-      <div>Debug : {newName}</div>
     </div>  
   )
 }
