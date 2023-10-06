a72e25e2be65dab1fb264e7e2c2f2a1c23646f6b
Thiwanka Somachandra <thivankas@outlook.com>
2023-10-06 10:54:39 +0300

    2.1 Excercise phonebook step1

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -2,19 +2,38 @@ import { useState } from 'react'
 
 const App = () => {
 
-  const [ person,setPerson ] = useState([{name : 'Alto Hellas'}]); 
-  const [ newName,setNewName ] = useState(''); 
+  const [ person,setPerson ] = useState([
+    {name : 'Alto Hellas'},
+    {name : 'Ada Lovelace'}
+  ]); 
+
+  const [ newName,setNewName ] = useState(' '); 
+
+  const handelchange = (event) => {
+    setNewName(event.target.value); 
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault(); 
+    const newPerson = {
+      name : newName
+    }
+    setPerson(person.concat(newPerson)); 
+    setNewName(' '); 
+  }
 
 
   return (
     <div>
       <h2>Phone Book</h2>
-  
-      <form>
-        <div> name : <input/> </div>
+      <form onSubmit={addPerson}>
+        <div> name : <input value={newName} onChange={handelchange}/> </div>
         <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
+      {person.map((person)=><p key={person.name}>{person.name}</p>)}
+      <br/>
+      <div>Debug : {newName}</div>
     </div>  
   )
 }
