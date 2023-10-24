bc3da313029f988e36de1ff69cd034848419db34
Thiwanka Somachandra <thivankas@outlook.com>
2023-10-24 05:25:51 +0300

    Remove Part3 Folder again

--- Part3/phonebook_frontend/src/components/Record.jsx
+++
@@ -1,8 +0,0 @@
-const Record = ({person,deleteRecord}) => {
-  const label = 'delete'; 
-    return (
-      <div>{person.map((record) => <p key={record.id}>{record.name} : {record.number} <button onClick={() => deleteRecord(record)}>{label}</button></p>)}</div>
-    )
-  } 
-
-  export default Record; 
