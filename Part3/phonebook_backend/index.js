46e0f8d64dc3ab9bd4a74a02369d57d6371e62ca
Thiwanka Somachandra <thivankas@outlook.com>
2023-10-27 23:06:09 +0300

    3.18 : Phonebook database step6

--- Part3/phonebook_backend/index.js
+++ Part3/phonebook_backend/index.js
@@ -20,30 +20,7 @@ const errorHandler = (error, request, response, next) => {
 }
 
 app.use(errorHandler); 
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]; 
-
+ 
 //get all the rcords from the DB 
 app.get('/api/persons',(request, response, next) => {
     Person.find({}).then(record => {
@@ -52,19 +29,21 @@ app.get('/api/persons',(request, response, next) => {
         next(error); 
     })
 })
-
-app.get('/info',(request,response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`);  
+//get the info
+app.get('/info',(request,response, next) => {
+    Person.find({}).then(records => {
+        response.send(`<p>Phonebook has info for ${records.length} people</p><p>${new Date()}</p>`);
+    }).catch(error => {
+        next(error); 
+    })  
 })
-
-app.get('/api/persons/:id',(request,response) => {
-    const id = Number(request.params.id); 
-    const person = persons.find(person => person.id === id); 
-    if(person){
-        response.json(person);
-    } else {
-        response.status(404).end(); 
-    }
+//get one record by Id
+app.get('/api/persons/:id',(request,response,next) => {
+    Person.findById(request.params.id).then(record => {
+        response.json(record);
+    }).catch(error => {
+        next(error);
+    })
 })
 //delete a record from DB
 app.delete('/api/persons/:id',(request,response,next) => {
