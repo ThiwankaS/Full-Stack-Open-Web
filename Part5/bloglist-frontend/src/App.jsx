cab7d50c24c14ac1254e61cdecbd07c038d9cb84
Thiwanka Somachandra <thivankas@outlook.com>
2023-11-30 04:13:54 +0200

    5.1: bloglist frontend, step1

--- Part5/bloglist-frontend/src/App.jsx
+++ Part5/bloglist-frontend/src/App.jsx
@@ -1,19 +1,56 @@
 import { useState,useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blog'
+import loginService from './services/login'
 
 const App = () => {
 
   const [ blogs,setBlogs] = useState([])
+  const [ user,setUser]   = useState(null)
+  const [ username,setUsername ] = useState('')
+  const [ password,setPassword ] = useState('')
+  const [ listToShow,setListToShow ] = useState([])
 
   useEffect(() => {
     blogService.getAll().then(blogs => setBlogs(blogs))
   },[])
 
+  const handelLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const loggedInUser = await loginService.login({ username,password })
+      setUser(loggedInUser)
+      const blogList = blogs.filter(record => record.user[0].username === loggedInUser.username)
+      setListToShow(blogList)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('wrong credentials')
+    }
+  }
+
+  if(user === null){
+    return(
+      <div>
+        <form onSubmit={handelLogin}>
+          <div>
+            username : 
+            <input type='text' value={username} name='Username' onChange={({target}) => setUsername(target.value)}/>
+          </div>
+          <div>
+            password : 
+            <input type='password' value={password} name='Password' onChange={({target}) => setPassword(target.value)}/>
+          </div>
+          <button type='submit'>Login</button>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog => <Blog key={blog.id} blog={blog}/>)}
+      {listToShow.map(blog => <Blog key={blog.id} blog={blog}/>)}
     </div>
   )
 }
