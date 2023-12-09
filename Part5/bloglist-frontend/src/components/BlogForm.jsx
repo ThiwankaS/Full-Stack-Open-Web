541beb950a0c2d1669467a47d722567856db90b1
Thiwanka Somachandra <thivankas@outlook.com>
2023-12-09 03:17:31 +0200

    5.16: Blog list tests, step4

--- Part5/bloglist-frontend/src/components/BlogForm.jsx
+++ Part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -24,15 +24,15 @@ const BlogForm = ({ createNew }) => {
       <form onSubmit={handleCreateNew}>
         <div>
             title :
-          <input type='text' value={title} name='Title' onChange={event => setTitle(event.target.value)}/>
+          <input placeholder='Title' type='text' value={title} name='Title' onChange={event => setTitle(event.target.value)}/>
         </div>
         <div>
             author :
-          <input type='text' value={author} name='Author' onChange={event => setAuthor(event.target.value)}/>
+          <input placeholder='Author' type='text' value={author} name='Author' onChange={event => setAuthor(event.target.value)}/>
         </div>
         <div>
             url :
-          <input type='text' value={url} name='Url' onChange={event => setUrl(event.target.value)}/>
+          <input placeholder='Url' type='text' value={url} name='Url' onChange={event => setUrl(event.target.value)}/>
         </div>
         <button type='submit'>Create</button>
       </form>
