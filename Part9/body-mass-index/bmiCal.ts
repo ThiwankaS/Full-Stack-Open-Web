6c24f3c4891ff5c7311d440d08afcf075a46dfe3
Thiwanka Somachandra <thivankas@outlook.com>
2024-03-15 06:29:33 +0200

    9.5 WebBMI

---
+++ Part9/body-mass-index/bmiCal.ts
@@ -0,0 +1,27 @@
+interface Result {
+    height : number;
+    weight : number;
+    bmi : string;
+}
+
+export const calculateBmi = ( h : number , w : number ) : Result => {
+    let index : number =  Math.round(((w) / (Math.pow((h/100),2)))*100)/100;
+    let result : string; 
+    switch(true){
+        case (index <= 18.4): 
+            result = ` Abnormal (under weight)`; 
+            break;
+        case (index <= 24.9): 
+            result = ` Normal (healthy weight)`; 
+            break; 
+        case (index <= 39.9): 
+            result = ` Abnormal (over weight)`; 
+            break; 
+        default : result = ` Abnormal (obese weight)`;
+    }
+    return {
+        height : h,
+        weight : w,
+        bmi : result
+    } 
+}
--- Part9/body-mass-index/index.ts
+++ Part9/body-mass-index/index.ts
@@ -1,9 +1,27 @@
 import express from 'express';
+import { calculateBmi } from './bmiCal';
 
 const app = express(); 
 
 app.get('/hello',(_request,response) => {
+
     response.send('Hello Full Stack!'); 
+})
+
+app.get('/bmi',(request,response) => {
+    if(request.query.height && request.query.weight){
+        const height : number = Number(request.query.height.toString());
+        const weight : number = Number(request.query.weight.toString());
+
+        if(isNaN(height) || isNaN(weight)){
+            return response.status(400).json({ " error " : " malformatted parameters " } );
+        }
+
+        const result = calculateBmi(height,weight); 
+        return response.send(result);
+    } else {
+        return response.status(400).json({ " error " : " malformatted parameters " } ); 
+    }
 })
 
 const PORT = 3003;
