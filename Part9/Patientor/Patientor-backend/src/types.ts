a3237787359ccc5ed0ceda4742d0198bd8f6caa9
Thiwanka Somachandra <thivankas@outlook.com>
2024-04-04 06:12:03 +0300

    9.22: Patientor, step 3

--- Part9/Patientor/Patientor-backend/src/types.ts
+++ Part9/Patientor/Patientor-backend/src/types.ts
@@ -1,11 +1,25 @@
 export type NonSensitivePatientEntries = Omit<Patient,'ssn' | 'entries'>;
 export type NonSensitivePatient = Omit<Patient,'ssn' | 'entries'>;
 export type NewPatientEntry = Omit<Patient,'id'>;
+export type Entry = HealthCheckEntry | OccupationalHealthcareEntry | HospitalEntry;
 
 export enum Gender {
     Male = 'male',
     Female = 'female',
     Other = 'other'
+}
+
+export enum Type {
+    Hospital = 'Hospital',
+    OccupationalHealthcare = 'OccupationalHealthcare',
+    HealthCheck = 'HealthCheck'
+}
+
+export enum  HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
 }
 
 export interface Diagnoses {
@@ -14,8 +28,38 @@ export interface Diagnoses {
     latin? : string;
 }
 
-export interface Entry {
+export interface SickLeave {
+    startDate : string;
+    endDate : string;
+}
 
+export interface Discharge {
+    date : string;
+    criteria : string;
+}
+
+export interface BaseEntry {
+    id : string;
+    date : string;
+    specialist : string;
+    description : string;
+    diagnosisCodes? : Array<Diagnoses['code']>;
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+    healthCheckRating : HealthCheckRating;
+    type : 'HealthCheck';
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+    employerName : string;
+    sickLeave : SickLeave;
+    type : 'OccupationalHealthcare';
+}
+
+export interface HospitalEntry extends BaseEntry {
+    discharge : Discharge;
+    type : 'Hospital';
 }
 
 export interface Patient {
--- Part9/Patientor/Patientor-backend/src/utils.ts
+++ Part9/Patientor/Patientor-backend/src/utils.ts
@@ -1,17 +1,17 @@
-import { NewPatientEntry,Gender } from "./types";
+import { NewPatientEntry,Gender,Entry} from "./types";
 
 const toNewPatientEntry = (object : unknown) : NewPatientEntry => {
     if( !object || typeof object !== 'object'){
         throw new Error('Incorrect or missing required data!');
     }
-    if( 'name' in object && 'ssn' in object && 'dateOfBirth' in object && 'occupation' in object && 'gender' in object){
+    if( 'name' in object && 'ssn' in object && 'dateOfBirth' in object && 'occupation' in object && 'gender' in object && 'entries' in object){
         const newEntry : NewPatientEntry = {
             name : parseName(object.name),
             ssn : parseSsn(object.ssn),
             dateOfBirth : parseBithDay(object.dateOfBirth),
             occupation : parseOccupation(object.occupation),
             gender : parseGender(object.gender),
-            entries : []
+            entries : parseEntries(object.entries)
         };
         return newEntry;
     }
@@ -55,6 +55,13 @@ const parseGender = (gender : unknown) : Gender => {
     return gender; 
 };
 
+const parseEntries = (entries : unknown) : Array<Entry> => {
+    if(!Array.isArray(entries) || !isType(entries as Array<Entry>)) {
+        throw new Error('Incorrect or missing types in entries!');
+    }
+    return entries as Array<Entry>;
+};
+
 const isString = (text : unknown) : text is string => {
     return typeof text === 'string' || text instanceof String;
 };
@@ -65,4 +72,9 @@ const isDate = (date : string) : boolean => {
 
 const isGender = (param : string) : param is Gender => {
     return Object.values(Gender).map( v => v.toString()).includes(param);
+};
+
+const isType = (param : Array<Entry>) : boolean => {
+    const validTypes = ["HealthCheck","OccupationalHealthcare","Hospital"];
+    return param.every(entry => validTypes.includes(entry.type));
 };
